9675da48e05212547c1e77f64a1a79736bcede62
apilsapkota <64177482+apilsapkota@users.noreply.github.com>
2021-03-20 11:34:46 -0400

    Add files via upload

---
+++ Spring-mvc-pr0/src/main/java/controllers/SagarDai.java
@@ -0,0 +1,13 @@
+package controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class SagarDai {
+	@GetMapping("/sagar")
+	public String sagar() {
+		return "hello";
+	}
+
+}
